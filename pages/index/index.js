689716ce440990f56381119b3f075b896623b921
Chnja <chj1997@whu.edu.cn>
2020-02-15 21:02:17 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,130 @@
+// pages/index/index.js
+const cweb = require('../../utils/cweb.js')
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    loading: 0,
+    name: "",
+    team: ''
+  },
+
+  inputchange(e) {
+    this.setData({
+      [e.currentTarget.dataset.name]: e.detail
+    })
+  },
+
+  bindteam() {
+    var team = this.data.team
+    if (team == '') {
+      return;
+    }
+    var that = this
+    cweb.cpost('/addteam', {
+      teamname: team
+    }).then(res => {})
+  },
+
+  bindname() {
+    var name = this.data.name
+    if (name == '') {
+      return;
+    }
+    var that = this
+    cweb.cpost('/personname', {
+      name: name
+    }).then(res => {
+      that.setData({
+        loading: 2
+      })
+    })
+  },
+
+  bindjoin() {
+    wx: wx.showModal({
+      title: '加入团队',
+      content: '点击微信群中的小程序卡片即可加入相应团队',
+      showCancel: false,
+      success: function(res) {},
+      fail: function(res) {},
+      complete: function(res) {},
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    var that = this
+    cweb.cpost('/launch', {}, false).then(res => {
+      if (res.code == '1000') {
+        that.setData({
+          loading: 1
+        })
+      } else if (res.teamlist.length == 0) {
+        that.setData({
+          loading: 2
+        })
+      } else {
+        wx: wx.redirectTo({
+          url: '/pages/team/team',
+          success: function(res) {},
+          fail: function(res) {},
+          complete: function(res) {},
+        })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  // onShareAppMessage: function() {
+
+  // }
+})
---
+++ utils/cweb.js
@@ -0,0 +1,60 @@
+const baseurl = 'http://127.0.0.1:5000'
+
+function cpost(url, data, loading = true) {
+  return new Promise(function(resolve, reject) {
+    if (loading) {
+      wx.showLoading({
+        title: '加载中',
+        mask: true,
+      })
+    }
+    wx: wx.login({
+      success: function(res) {
+        data.code = res.code
+        wx: wx.request({
+          url: baseurl + url,
+          data: data,
+          header: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          },
+          method: 'POST',
+          success: function(res) {
+            if (loading) {
+              wx: wx.hideLoading()
+            }
+            console.log(res.data)
+            if (res.data.code < 2000) {
+              resolve(res.data)
+            } else {
+              wx: wx.showModal({
+                title: '提示',
+                content: '发生错误，错误代码：' + res.data.code,
+                showCancel: false,
+              })
+              reject()
+            }
+          },
+          fail: function(res) {
+            if (loading) {
+              wx: wx.hideLoading()
+            }
+            wx: wx.showModal({
+              title: '提示',
+              content: '网络连接失败',
+              showCancel: false,
+            })
+            reject()
+          },
+          complete: function(res) {},
+        })
+      },
+      fail: function(res) {},
+      complete: function(res) {},
+    })
+  })
+}
+
+
+module.exports = {
+  cpost: cpost,
+}
