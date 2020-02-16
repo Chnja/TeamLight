cdef9305bd1cf02fa1bf33683bf9dedfdac8fc54
Chnja <chj1997@whu.edu.cn>
2020-02-17 00:29:29 +0800

    teamdetail-50%

--- pages/teamdetail/teamdetail.js
+++ pages/teamdetail/teamdetail.js
@@ -1,31 +1,132 @@
 // pages/teamdetail/teamdetail.js
+const cweb = require('../../utils/cweb.js')
+var teamid = ''
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    option: [{
-        text: '全部商品',
-        value: 0
-      },
-      {
-        text: '新款商品',
-        value: 1
-      },
-      {
-        text: '活动商品',
-        value: 2
-      }
-    ],
-    value: 0,
+    teamid: '',
+    show: {},
+    person: {},
+    popup: false,
+    popbottom: false,
+    name: '',
+    team: ''
+  },
+
+  bindname() {
+    var that = this
+    cweb.cpost('/personname', {
+      name: this.data.name
+    }).then(res => {
+      that.setData({
+        popup: false,
+      })
+      that.loadperson().then(() => {
+        that.loadfun()
+      })
+    })
+  },
+
+  bindteam() {
+    var that = this
+    cweb.cpost('/teamname', {
+      team: this.data.team,
+      teamid: teamid
+    }).then(res => {
+      that.setData({
+        popbottom: false,
+      })
+      that.loadperson().then(() => {
+        that.loadfun()
+      })
+    })
+  },
+
+  bindup() {
+    this.setData({
+      popup: !this.data.popup,
+      name: this.data.person.name
+    })
+  },
+
+  bindbottom() {
+    this.setData({
+      popbottom: !this.data.popbottom,
+      team: this.data.show.name
+    })
+  },
+
+  inputchange(e) {
+    this.setData({
+      [e.currentTarget.dataset.name]: e.detail
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    teamid = options.teamid
+    var that = this
+    this.loadperson().then(() => {
+      that.loadfun()
+    })
+    // this.loadteam()
+  },
 
+  loadperson() {
+    var that = this
+    return new Promise(function(resolve, reject) {
+      cweb.cpost('/launch', {}).then(res => {
+        if (res.code == 1000) {
+          wx: wx.redirectTo({
+            url: '/pages/index/index',
+            success: function(res) {},
+            fail: function(res) {},
+            complete: function(res) {},
+          })
+        }
+        else {
+          var flag = false
+          for (var i of res.teamlist) {
+            i.text = i.name
+            i.value = i._id
+            if (teamid == i._id) {
+              flag = true
+            }
+          }
+          if (!flag) {
+            wx: wx.redirectTo({
+              url: '/pages/team/team?teamid=' + teamid,
+              success: function(res) {},
+              fail: function(res) {},
+              complete: function(res) {},
+            })
+          }
+          that.setData({
+            person: res,
+            teamid: teamid * 1
+          })
+          resolve()
+        }
+      })
+    })
+  },
+
+  loadfun() {
+    var that = this
+    return new Promise(function(resolve, reject) {
+      cweb.cpost('/teampeople', {
+        'teamid': teamid
+      }).then(res => {
+        that.setData({
+          show: res
+        })
+        resolve()
+      })
+    })
   },
 
   /**
@@ -39,7 +140,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-
+    wx.hideHomeButton()
   },
 
   /**
@@ -60,7 +161,14 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function() {
-
+    wx.showNavigationBarLoading()
+    var that = this
+    this.loadperson().then(() => {
+      that.loadfun().then(() => {
+        wx.stopPullDownRefresh()
+        wx.hideNavigationBarLoading()
+      })
+    })
   },
 
   /**
